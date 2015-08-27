5a4e81a72fe3a04d3fe64f52727f0de7c6fbe172
filatasjon <filatasjon@gmail.com>
2015-08-27 17:47:03 -0300

    Tela importação XML, botão detlahes.

--- js/utils.js
+++ js/utils.js
@@ -142,8 +142,8 @@ angular.module('utils', [ ])
   
   // URL base da WEBAPI
   //var urlBase = 'http://localhost:55007';
-  //var urlBase = 'http://192.168.0.100/ws/proxy';  // proxy: versão mais atualizada da webapi 
-  var urlBase = 'http://ws.atoscapital.com.br/proxy';
+  var urlBase = 'http://192.168.0.100/ws/proxy';  // proxy: versão mais atualizada da webapi 
+  //var urlBase = 'http://ws.atoscapital.com.br/proxy';
   // Tempo em horas máximos definido de inatividade para requerer novo login, caso LEMBRAR não tenha sido marcado
   const HORAS_NOVO_LOGIN = 2;
     
