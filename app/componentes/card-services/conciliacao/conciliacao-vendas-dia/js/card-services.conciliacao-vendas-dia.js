ac6decabb9128620139ab51a3b572a0166a04e1c
LuizFelipe12 <luizfelipems12@gmail.com>
2015-08-28 17:48:19 -0300

    Alteracoes na tela de Conciliacao vendas dia

--- app/componentes/card-services/conciliacao/conciliacao-vendas-dia/js/card-services.conciliacao-vendas-dia.js
+++ app/componentes/card-services/conciliacao/conciliacao-vendas-dia/js/card-services.conciliacao-vendas-dia.js
@@ -13,6 +13,7 @@ angular.module("card-services-conciliacao-vendas-dia", [])
     // flags
     $scope.exibeTela = false;
     // Data
+    $scope.emitentes = [];
     $scope.datamin = new Date();
     $scope.datamax = null;
     $scope.abrirCalendarioDataMin = false;
@@ -66,5 +67,53 @@ angular.module("card-services-conciliacao-vendas-dia", [])
     $scope.limpaDataMax = function () {
         $scope.datamax = null;
       };
+        
+    // TABS
+    /**
+      * Seta a tab
+      */
+    $scope.setTab = function(tab){
+        if(typeof tab === 'number' && tab >= 0 && tab < 3){ 
+            $scope.tab = tab;
+            if($scope.tabIs(0)){
+                $scope.paginaInformada = $scope.filtro.terminal.pagina;
+                if($scope.relatorio.terminal.length == 0 && 
+                   ($scope.relatorio.sintetico.length > 0 || 
+                    $scope.relatorio.analitico.length > 0)) 
+                        buscaRelatorioTerminal(false);   
+            }else if($scope.tabIs(1)){
+                $scope.paginaInformada = $scope.filtro.sintetico.pagina;
+                if($scope.relatorio.sintetico.length == 0 && 
+                   ($scope.relatorio.terminal.length > 0 || 
+                    $scope.relatorio.analitico.length > 0)) 
+                        buscaRelatorioSintetico(false);   
+            }else if($scope.tabIs(2)){
+                $scope.paginaInformada = $scope.filtro.analitico.pagina;
+                if($scope.relatorio.analitico.length == 0 && 
+                   ($scope.relatorio.terminal.length > 0 || 
+                    $scope.relatorio.sintetico.length > 0)) 
+                        buscaRelatorioAnalitico(false, false);   
+            }
+        }
+    }
+    
+        // EMITENTE
+    $scope.toggle = function(emitente){
+        if(!emitente || emitente === null) return;
+        if(emitente.collapsed) emitente.collapsed = false;
+        else emitente.collapsed = true;
+    }
+    $scope.isExpanded = function(emitente){
+        if(!emitente || emitente === null) return;
+        return emitente.collapsed;
+    }
+    
+    /**
+      * Retorna true se a tab selecionada é inforamda
+      */
+    $scope.tabIs = function(tab){
+        return $scope.tab === tab;
+    }
+    
     
 }]);
