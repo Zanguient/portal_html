c68e8ba5cb82fba58e41a0bbb873a584c57e260e
deividgfmarinho <deividgfmarinho@gmail.com>
2015-09-11 21:12:04 -0300

    Modal Certificado Digital

--- js/utils.js
+++ js/utils.js
@@ -148,8 +148,8 @@ angular.module('utils', [ ])
   
   // URL base da WEBAPI
   //var urlBase = 'http://localhost:55007';
-  var urlBase = 'http://192.168.1.100/apiportal';  // proxy: versão mais atualizada da webapi 
-  //var urlBase = 'http://ws.atoscapital.com.br/proxy';
+  //var urlBase = 'http://192.168.1.100/apiportal';  // proxy: versão mais atualizada da webapi 
+  var urlBase = 'http://ws.atoscapital.com.br/proxy';
   // IMessage
   //var urlBaseIMessage = 'http://localhost:50780';
   var urlBaseIMessage = 'http://imessage.atoscapital.com.br';
