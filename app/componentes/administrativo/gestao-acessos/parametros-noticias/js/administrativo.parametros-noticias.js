2932a55d49bfcac310ba074774fb98760115e0c8
filatasjon <filatasjon@gmail.com>
2015-10-02 12:08:16 -0300

    Parêmetros de noticias.

--- app/componentes/administrativo/gestao-acessos/parametros-noticias/js/administrativo.parametros-noticias.js
+++ app/componentes/administrativo/gestao-acessos/parametros-noticias/js/administrativo.parametros-noticias.js
@@ -29,36 +29,24 @@ angular.module("administrativo-parametros-noticias", [])
     $scope.administrativoParametrosNoticiasInit = function(){
         // Título da página 
         $scope.pagina.titulo = 'Gestão de Acessos';                          
-        $scope.pagina.subtitulo = 'Usuários';
+        $scope.pagina.subtitulo = 'Parâmetros Bancários';
         // Quando houver uma mudança de rota => modificar estado
         $scope.$on('mudancaDeRota', function(event, state, params){
             $state.go(state, params);
         });
-        // Quando houver alteração do grupo empresa na barra administrativa                                           
-        $scope.$on('alterouGrupoEmpresa', function(event){
-            if($scope.exibeTela){
-                // Modifica a visibilidade do campo de busca para o grupo empresa
-                $scope.camposBusca[2].ativo = !$scope.usuariologado.grupoempresa;   
-                // Refaz a busca
-                $scope.buscaUsuarios();
-            }
-        }); 
-        // Obtém as permissões
-        if($scope.methodsDoControllerCorrente){// && $scope.methodsDoControllerCorrente.length > 0){
-            permissaoAlteracao = $scope.methodsDoControllerCorrente['atualização'] ? true : false;//$filter('filter')($scope.methodsDoControllerCorrente, function(m){ return m.ds_method.toUpperCase() === 'ATUALIZAÇÃO' }).length > 0;   
-            permissaoCadastro = $scope.methodsDoControllerCorrente['cadastro'] ? true : false;//$filter('filter')($scope.methodsDoControllerCorrente, function(m){ return m.ds_method.toUpperCase() === 'CADASTRO' }).length > 0;
-            permissaoRemocao = $scope.methodsDoControllerCorrente['remoção'] ? true : false;//$filter('filter')($scope.methodsDoControllerCorrente, function(m){ return m.ds_method.toUpperCase() === 'REMOÇÃO' }).length > 0;
-        }
+        
         // Quando o servidor for notificado do acesso a tela, aí sim pode exibí-la  
         $scope.$on('acessoDeTelaNotificado', function(event){
             $scope.exibeTela = true;
             // Busca Usuários
-            $scope.buscaUsuarios();
+            $scope.buscaCatalogos();
         });
         // Acessou a tela
         $scope.$emit("acessouTela");
         // Busca Usuários
         //$scope.buscaUsuarios();
+        
+        
     };
     
        
