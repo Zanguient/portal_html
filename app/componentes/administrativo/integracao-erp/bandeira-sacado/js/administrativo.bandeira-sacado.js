96c54a68ee02fcca237f8c2acecc46ff66012d77
Jeremias Santos <jeremiassnts3@gmail.com>
2016-05-12 11:51:06 -0300

    Tela Bandeira Sacado: edição e cadastro finalizado

--- app/componentes/administrativo/integracao-erp/bandeira-sacado/js/administrativo.bandeira-sacado.js
+++ app/componentes/administrativo/integracao-erp/bandeira-sacado/js/administrativo.bandeira-sacado.js
@@ -26,6 +26,9 @@ angular.module("administrativo-bandeira-sacado", []).controller("administrativo-
     // Filtros
     $scope.adquirentes = [];
     $scope.bandeiras = [];
+	$scope.bandeirasAlteracao = [];
+	$scope.bandeirasCadastro = [];
+	$scope.bandeirasFixo = [];
 	$scope.sacado = "";
 	$scope.bandeiraSacado = [];
 	$scope.statusLogado = false;
@@ -35,11 +38,11 @@ angular.module("administrativo-bandeira-sacado", []).controller("administrativo-
     var divPortletFiltroPos = 0; // posição da div que vai receber o loading progress
 	var divPortletBodyPos = 1; // posição da div que vai receber o loading progress
     // Cadastro
-    $scope.cadastro = {adquirente : '', sacado : 0, parcelas : 0, nmBandeira : ''};                                       
+    $scope.cadastro = {adquirente : "", sacado : "", parcelas : 0, bandeira : { cdBandeira : 0, nmBandeira : '' }};
     // Alteração
     $scope.alterando = true;                                             
-    $scope.alteracao = {id : 0, sacado : 0, parcelas : 0, nmBandeira : ''};                                             
-	$scope.statusAlteracao = false;
+    $scope.alteracao = {id : 0, grupo : 0, adquirente : null, sacado : "", parcelas : 0, bandeira : { cdBandeira : 0, nmBandeira : '' }};     
+	$index = 0;
     // flag
     $scope.exibePrimeiraLinha = false; 
     $scope.exibeTela = false;                                             
@@ -65,6 +68,7 @@ angular.module("administrativo-bandeira-sacado", []).controller("administrativo-
 					if($scope.usuariologado.grupoempresa){ 
 						// Reseta seleção de filtro específico de empresa
 						buscaAdquirentes();
+						buscaBandeiraFixo();
 						buscaBandeira();
 						$scope.sacado = null;
 						$scope.statusLogado = true;
@@ -81,6 +85,7 @@ angular.module("administrativo-bandeira-sacado", []).controller("administrativo-
 						$scope.filtro.faixa_registros = '0-0';
 						$scope.filtro.pagina = 1;
 						$scope.filtro.total_paginas = 0;
+						$scope.filtro.total_registros = 0;
 					}
 				}
 			}); 
@@ -91,6 +96,7 @@ angular.module("administrativo-bandeira-sacado", []).controller("administrativo-
 				if($scope.usuariologado.grupoempresa){ 
 					// Reseta seleção de filtro específico de empresa
 					buscaAdquirentes();
+					buscaBandeiraFixo();
 					buscaBandeira();
 					$scope.sacado = null;
 					$scope.statusLogado = true;
@@ -107,6 +113,7 @@ angular.module("administrativo-bandeira-sacado", []).controller("administrativo-
 					$scope.filtro.faixa_registros = '0-0';
 					$scope.filtro.pagina = 1;
 					$scope.filtro.total_paginas = 0;
+					$scope.filtro.total_registros = 0;
 				}
 			});
 			// Obtém as permissões
@@ -234,6 +241,84 @@ angular.module("administrativo-bandeira-sacado", []).controller("administrativo-
 			}
 		};
 		
+		var buscaBandeiraFixo = function(showMessage){
+			
+			if($scope.usuariologado.grupoempresa){
+				
+				var filtros = [];					
+				$webapi.get($apis.getUrl($apis.card.tbbandeira, [$scope.token, 1, /*$campos.card.tbbandeira.dsBandeira*/ 101], filtros)) 
+					.then(function(dados){
+					//console.log(dados.Registros);
+					$scope.bandeirasFixo = dados.Registros;
+				},
+				function(failData){
+					if(failData.status === 0) $scope.showAlert('Falha de comunicação com o servidor', true, 'warning', true); 
+					else if(failData.status === 503 || failData.status === 404) $scope.voltarTelaLogin(); // Volta para a tela de login
+					else $scope.showAlert('Houve uma falha ao obter bandeiras (' + failData.status + ')', true, 'danger', true);
+				});
+			}
+		};
+		
+		$scope.alterouAdquirenteAlteracao = function(){
+			if($scope.alteracao.adquirente != null) buscaBandeiraAlteracao();
+		}
+		
+		var buscaBandeiraAlteracao = function(showMessage){
+			
+			if($scope.usuariologado.grupoempresa){
+				
+				//$scope.showProgress(divPortletFiltroPos); 
+				var filtros = [];
+				if($scope.alteracao.adquirente && $scope.alteracao.adquirente !== null){
+					filtros.push({id:/*$campos.card.tbbandeira.cdAdquirente*/ 102,
+					valor: $scope.alteracao.adquirente.cdAdquirente});
+				}
+					
+				$webapi.get($apis.getUrl($apis.card.tbbandeira, [$scope.token, 1, /*$campos.card.tbbandeira.dsBandeira*/ 101], filtros)) 
+					.then(function(dados){
+					//console.log(dados.Registros);
+					$scope.bandeirasAlteracao = dados.Registros;
+					//$scope.hideProgress(divPortletFiltroPos);
+				},
+				function(failData){
+					if(failData.status === 0) $scope.showAlert('Falha de comunicação com o servidor', true, 'warning', true); 
+					else if(failData.status === 503 || failData.status === 404) $scope.voltarTelaLogin(); // Volta para a tela de login
+					else $scope.showAlert('Houve uma falha ao obter bandeiras (' + failData.status + ')', true, 'danger', true);
+					//$scope.hideProgress(divPortletBodyFiltrosPos);
+				});
+			}
+		};
+		
+		$scope.alterouAdquirenteCadastro = function(){
+			if($scope.cadastro.adquirente != null) buscaBandeiraCadastro();
+		}
+		
+		var buscaBandeiraCadastro = function(showMessage){
+			
+			if($scope.usuariologado.grupoempresa){
+				
+				//$scope.showProgress(divPortletFiltroPos); 
+				var filtros = [];
+				if($scope.cadastro.adquirente && $scope.cadastro.adquirente !== null){
+					filtros.push({id:/*$campos.card.tbbandeira.cdAdquirente*/ 102,
+					valor: $scope.cadastro.adquirente.cdAdquirente});
+				}
+					
+				$webapi.get($apis.getUrl($apis.card.tbbandeira, [$scope.token, 1, /*$campos.card.tbbandeira.dsBandeira*/ 101], filtros)) 
+					.then(function(dados){
+					//console.log(dados.Registros);
+					$scope.bandeirasCadastro = dados.Registros;
+					//$scope.hideProgress(divPortletFiltroPos);
+				},
+				function(failData){
+					if(failData.status === 0) $scope.showAlert('Falha de comunicação com o servidor', true, 'warning', true); 
+					else if(failData.status === 503 || failData.status === 404) $scope.voltarTelaLogin(); // Volta para a tela de login
+					else $scope.showAlert('Houve uma falha ao obter bandeiras (' + failData.status + ')', true, 'danger', true);
+					//$scope.hideProgress(divPortletBodyFiltrosPos);
+				});
+			}
+		};
+		
 		$scope.alterouAdquirente = function(){
 			$scope.filtro.bandeira = null;
 			buscaBandeira();
@@ -245,13 +330,24 @@ angular.module("administrativo-bandeira-sacado", []).controller("administrativo-
 			$scope.filtro.sacado = null;
 			buscaAdquirentes();
 			buscaBandeira();
+			buscaBandeiraSacado();
 		};
+		
+		$scope.buscaDados = function(){
+			if($scope.filtro.sacado != null){
+				if($scope.filtro.sacado.trim().length == 0){
+					$scope.filtro.sacado = null;
+				}
+			}
+			buscaBandeiraSacado();
+		}
 		
 		var buscaBandeiraSacado = function(showMessage){
 			if($scope.usuariologado.grupoempresa){
 				
 				$scope.showProgress(divPortletBodyPos); 
 				var filtros = [];
+				
 				if($scope.filtro.bandeira && $scope.filtro.bandeira !== null){
 					filtros.push({id:/*$campos.card.tbbandeirasacado.cdBandeira*/ 101,
 								  valor: $scope.filtro.bandeira.cdBandeira});
@@ -260,9 +356,10 @@ angular.module("administrativo-bandeira-sacado", []).controller("administrativo-
 					filtros.push({id:/*$campos.card.tbbandeirasacado.cdSacado*/ 103,
 								  valor: $scope.filtro.sacado});
 				}
+				
 					
 				$webapi.get($apis.getUrl($apis.card.tbbandeirasacado,
-				[$scope.token, 2, /*$campos.card.tbbandeirasacado.tbbandeirasacado*/ 103, 0,
+				[$scope.token, 1, /*$campos.card.tbbandeirasacado.cdBandeira*/ 101, 0,
 				$scope.filtro.itens_pagina, $scope.filtro.pagina],
 				filtros))
 					.then(function(dados){
@@ -279,7 +376,7 @@ angular.module("administrativo-bandeira-sacado", []).controller("administrativo-
 					// Verifica se a página atual é maior que o total de páginas
 					if($scope.filtro.pagina > $scope.filtro.total_paginas)
 						setPagina(1); // volta para a primeira página e refaz a busca
-					
+					$scope.alteracao.id = $scope.filtro.total_registros;
 					// Esconde o progress
 					$scope.hideProgress(divPortletBodyPos);
 				},
@@ -298,23 +395,168 @@ angular.module("administrativo-bandeira-sacado", []).controller("administrativo-
 			else return null; 
 		};
 		
-		//ALTERA DADOS BANDEIRA SACADO
-		$scope.editarBandeiraSacado = function(item){
+		$scope.formataBandeira = function(codigo){
+			var nmBandeira = '';
+			if(codigo != null){
+				angular.forEach($scope.bandeirasFixo, function(bandeira){
+					if(bandeira.cdBandeira == codigo)
+						nmBandeira = bandeira.dsBandeira;
+				});
+				return nmBandeira;
+			}
+			else return null;
+		};
+		
+		//EDITA DADOS BANDEIRA SACADO
+		$scope.editarBandeiraSacado = function(item, index){
 			// Reseta os valores
+			$scope.alteracao.id = index;
+			$scope.alteracao.grupo = item.cdGrupo;
 			$scope.alteracao.sacado = item.cdSacado; 
 			$scope.alteracao.parcelas = item.qtParcelas;
-			$scope.alteracao.nmBandeira = item.tbBandeira.dsBandeira;
+			$scope.alteracao.bandeira.cdBandeira = item.cdBandeira;
+			//$scope.alteracao.bandeira.cdBandeira = item.tbBandeira.cdBandeira;
+			//$scope.alteracao.bandeira.nmBandeira = item.tbBandeira.dsBandeira;
 			// Exibe na linha
 			$scope.alterando = true;
-			$scope.statusAlteracao = true;       
 		};
 		
 		//CANCELA ALTERAÇÃO
 		$scope.cancelaAlteracao = function(){
-			$scope.statusAlteracao = false;
+			$scope.alteracao.id = $scope.filtro.total_registros;
 			$scope.alterando = false; 
+			$scope.alteracao.adquirente = null;
+			buscaBandeiraAlteracao();
+			//$scope.alteracao.bandeira = null;
 		};
 		
+		//ALTERA DADOS BANDEIRA SACADO
+		$scope.alteraBandeiraSacado = function(item){
+			if(typeof item === 'undefined') return;
+			
+			if($scope.alteracao.adquirente === null){
+			   $scope.showModalAlerta('Selecione uma adquirente!');
+			   return false;
+			}
+		   
+		   if($scope.alteracao.bandeira === null){
+			   $scope.showModalAlerta('Selecione uma adquirente!');
+			   return false;
+			}
+			
+			if($scope.alteracao.sacado.trim().length == 0){
+			   $scope.showModalAlerta('É necessário digitar o sacado!');
+			   return false;
+			}
+			   
+		   if($scope.alteracao.parcelas == null){
+				$scope.showModalAlerta('É necessário digitar a quantidade de parcelas!');
+			   return false;   
+			}
+		   
+			// Altera
+			$scope.showProgress(divPortletFiltroPos); // z-index < z-index do fullscreen    
+			$scope.showProgress(divPortletBodyPos);
+			var jsonBandeiraSacado = { 
+				cdGrupo :  $scope.usuariologado.grupoempresa &&  $scope.usuariologado.grupoempresa !== null ? $scope.usuariologado.grupoempresa.id_grupo : 0,// grupo associado
+				oldCdSacado : item.cdSacado, // cdSacado que está armazenado na base
+				//oldCdBandeira : item.tbBandeira.cdBandeira, // bandeira que está armazenada na base
+				oldCdBandeira : item.cdBandeira, // bandeira que está armazenada na base
+				oldQtParcelas : item.qtParcelas, // parcelas que  está armazenado na base
+				newCdSacado : $scope.alteracao.sacado, // sacado informado na alteração
+				newCdBandeira : $scope.alteracao.bandeira.cdBandeira, // bandeira informada na alteração
+				newQtParcelas : $scope.alteracao.parcelas // parcelas informada na alteração
+			};
+        
+			$webapi.update($apis.getUrl($apis.card.tbbandeirasacado, undefined,
+                                   {id : 'token', valor : $scope.token}), jsonBandeiraSacado) 
+            .then(function(dados){           
+                $scope.showAlert('Alterado com sucesso!', true, 'success', true);
+                // Esconde a linha
+                $scope.cancelaAlteracao();
+                // Relista
+                buscaBandeiraSacado();
+				$scope.hideProgress(divPortletFiltroPos);
+              },
+              function(failData){
+                 if(failData.status === 0) $scope.showAlert('Falha de comunicação com o servidor', true, 'warning', true); 
+                 else if(failData.status === 503 || failData.status === 404) $scope.voltarTelaLogin(); // Volta para a tela de login
+                 else $scope.showAlert('Houve uma falha ao atualizar os dados (' + failData.status + ')', true, 'danger', true);
+                 // Fecha os progress
+                 $scope.hideProgress(divPortletFiltroPos);
+                 $scope.hideProgress(divPortletBodyPos);
+              });  
+		};
+		
+		//CADASTRO
+		$scope.novoBandeiraSacado = function(){
+			// Reseta os valores
+			$scope.cadastro.adquirente = null;
+			$scope.cadastro.bandeira = null;
+			$scope.cadastro.sacado = '';
+			$scope.cadastro.parcelas = 0;
+			// Exibe a linha
+			$scope.exibePrimeiraLinha = true;        
+		};
+		
+		$scope.cancelarNovo = function(){
+			$scope.exibePrimeiraLinha = false;    
+		};
+		
+		 var validaBandeiraSacado = function(){
+		   if($scope.cadastro.adquirente === null){
+			   $scope.showModalAlerta('Selecione uma adquirente!');
+			   return false;
+		   }
+		   
+		   if($scope.cadastro.bandeira === null){
+			   $scope.showModalAlerta('Selecione uma adquirente!');
+			   return false;
+		   }
+		   
+		   if($scope.cadastro.sacado.trim().length == 0){
+			   $scope.showModalAlerta('É necessário digitar o sacado!');
+			   return false;
+		   }
+			   
+		   if($scope.cadastro.parcelas == null){
+				$scope.showModalAlerta('É necessário digitar a quantidade de parcelas!');
+			   return false;   
+		   }
+		   
+		   return true;
+		};
+		
+		$scope.salvarBandeiraSacado = function(){
+		   if(!validaBandeiraSacado()) return;    
+		   // Cadastra
+		   $scope.showProgress(divPortletFiltroPos, 10000); // z-index < z-index do fullscreen    
+		   $scope.showProgress(divPortletBodyPos);
+		   var jsonBandeiraSacadoSalvar = {
+				cdGrupo : $scope.usuariologado.grupoempresa &&  $scope.usuariologado.grupoempresa !== null ? $scope.usuariologado.grupoempresa.id_grupo : 0,// grupo associado
+				cdSacado : $scope.cadastro.sacado,//novo sacado
+				qtParcelas : $scope.cadastro.parcelas,//nova parcela
+				cdBandeira : $scope.cadastro.bandeira.cdBandeira//nova bandeira
+			}; 
+			
+		   $webapi.post($apis.getUrl($apis.card.tbbandeirasacado, undefined,
+									 {id : 'token', valor : $scope.token}), jsonBandeiraSacadoSalvar) 
+				.then(function(dados){           
+					$scope.showAlert('Cadastrado com sucesso!', true, 'success', true);
+					// Esconde a linha
+					$scope.exibePrimeiraLinha = false;
+					// Relista
+					buscaBandeiraSacado();
+					$scope.hideProgress(divPortletFiltroPos);
+				  },
+				  function(failData){
+					 if(failData.status === 0) $scope.showAlert('Falha de comunicação com o servidor', true, 'warning', true); 
+					 else if(failData.status === 503 || failData.status === 404) $scope.voltarTelaLogin(); // Volta para a tela de login
+					 else $scope.showAlert('Houve uma falha ao realizar o cadastro (' + failData.status + ')', true, 'danger', true);
+					 // Fecha os progress
+					 $scope.hideProgress(divPortletFiltroPos);
+					 $scope.hideProgress(divPortletBodyPos);
+				  }); 
+			};		
 	}]);
-	/*
-	*/
+	
